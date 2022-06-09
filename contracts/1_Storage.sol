fa5c1107b9a0bc390469273bf5f67da3506bb05a
dihsarjr <mohammed1rashid786@gmail.com>
2022-06-10 01:50:30 +0530

    new

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -2,28 +2,30 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-/**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
- */
-contract Storage {
 
-    uint256 number;
+contract Todo {
 
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
-    function store(uint256 num) public {
-        number = num;
-    }
+    //this list will hold all todos
+   TodoModel[] todoList;
 
-    /**
-     * @dev Return value 
-     * @return value of 'number'
-     */
-    function retrieve() public view returns (uint256){
-        return number;
-    }
+    // this function helps to get all todos
+   function getTodoList() public view returns(TodoModel[] memory){
+       return todoList;
+   }
+
+    // this function helps to add todos to [todoList]
+   function addTodo(string memory todoName) public {
+    todoList.push(TodoModel(false,todoName));
+   }
+
+    // this function helps to change state of single todo
+   function changeToDoState(bool state,uint256 index) public {
+    todoList[index].isComplete = state;
+   }
+
+}
+
+struct TodoModel{
+    bool isComplete;
+    string todoName;
 }
